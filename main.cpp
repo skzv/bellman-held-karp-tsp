7f9d7766d5a21b0e03556691eb674b5fbd926e8b
skz Kuznetsov <me@skz.dev>
2022-08-21 14:19:31 -0700

    Switch edges to double and add data

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <unordered_set>
 #include <map>
 #include <algorithm>
+#include <fstream>
 
 class UndirectedGraph {
 public:
-    void addEdge(int u, int v, float c) {
+    void addEdge(int u, int v, double c) {
         vertices.insert(u);
         vertices.insert(v);
         edges[{u, v}] = c;
@@ -19,9 +20,9 @@ public:
         return edges.size();
     };
 
-    float getEdgeCost(int u, int v) {
+    double getEdgeCost(int u, int v) {
         // since this is an undirected graph we check for an edge in both {u, v} or {v, u} directions
-        std::map<std::pair<int, int>, float>::iterator edge;
+        std::map<std::pair<int, int>, double>::iterator edge;
 
         // check for u,v
         edge = edges.find({u, v});
@@ -36,16 +37,16 @@ public:
         }
 
         // else return +inf
-        return std::numeric_limits<float>::max();;
+        return std::numeric_limits<double>::max();;
     }
 
 private:
     std::unordered_set<int> vertices;
-    std::map<std::pair<int, int>, float> edges;
+    std::map<std::pair<int, int>, double> edges;
 };
 
-/** Binary representation of a subset S of vector space V = {1, 2, ..., n} of size n where inclusion of element i is represented by the bit
- * at the i'th position. */
+/** Binary representation of a subset S of vector space V = {1, 2, ..., n} of size n where inclusion of element i is
+ * represented by the bit at the i'th position. */
 bool isElementInSet(int element, int set) {
     return ((set >> element) & 1) == 1;
 }
@@ -57,8 +58,8 @@ int removeElementFromSet(int element, int set) {
     return set;
 }
 
-/** Binary representation of a subset S of vector space V = {1, 2, ..., n} of size n where inclusion of element i is represented by the bit
- * at the i'th position. */
+/** Binary representation of a subset S of vector space V = {1, 2, ..., n} of size n where inclusion of element i is
+ * represented by the bit at the i'th position. */
 std::unordered_set<int> generateAllSubsetsOfSize(int vectorSpaceSize, int subsetSize) {
     // memoize this
     std::unordered_set<int> sets;
@@ -88,8 +89,8 @@ std::unordered_set<int> generateAllSubsetsOfSize(int vectorSpaceSize, int subset
     return sets;
 }
 
-/** Binary representation of a subset S of vector space V = {1, 2, ..., n} of size n where inclusion of element i is represented by the bit
- * at the i'th position. */
+/** Binary representation of a subset S of vector space V = {1, 2, ..., n} of size n where inclusion of element i is
+ * represented by the bit at the i'th position. */
 std::unordered_set<int> generateAllSubsetsIncludingFirstElementOfSize(int vectorspaceSize, int subsetSize) {
     std::unordered_set<int> bitmasks = generateAllSubsetsOfSize(vectorspaceSize - 1,
                                                                 subsetSize - 1);
@@ -102,14 +103,14 @@ std::unordered_set<int> generateAllSubsetsIncludingFirstElementOfSize(int vector
 }
 
 /** Returns the shortest path distance that visits every node once. */
-float BellmanHeldKarpTsp(UndirectedGraph g) {
+double BellmanHeldKarpTsp(UndirectedGraph g) {
     int n = g.getNumVertices();
     int V = (int) pow(2, n - 1) - 1;
 
     // index vertices from 0 to (n - 1)
     // sub-problems (1 e S, |S| >= 2, j e V - {0})
     // (only sub-problems with j e S are ever used)
-    float A[V][n - 1];
+    double A[V][n - 1];
 
     // base cases (|S| = 2)
     for (int j = 1; j < n; j++) {
@@ -125,7 +126,7 @@ float BellmanHeldKarpTsp(UndirectedGraph g) {
                 // if j is in set S
                 if (isElementInSet(j, S)) {
                     // search for minPath from k (where k is in S - {j}, k != 1, j) to j
-                    float minPath = std::numeric_limits<float>::max();
+                    double minPath = std::numeric_limits<double>::max();
                     for (int k = 1; k < n; k++) {
                         if (k == j) {
                             continue;
@@ -142,7 +143,7 @@ float BellmanHeldKarpTsp(UndirectedGraph g) {
     }
 
     // compute optimal tour cost
-    float minCost = std::numeric_limits<float>::max();
+    double minCost = std::numeric_limits<double>::max();
     for (int j = 1; j < n; j++) {
         minCost = std::min(minCost, A[V][j] + g.getEdgeCost(j, 0));
     }
@@ -153,12 +154,25 @@ UndirectedGraph readGraph() {
     // TODO: read graph from input file
     UndirectedGraph g;
 
+    // hardcoded values for testing
     g.addEdge(0, 1, 1);
     g.addEdge(1, 2, 5);
     g.addEdge(2, 3, 2);
     g.addEdge(3, 0, 7);
     g.addEdge(1, 3, 3);
     g.addEdge(0, 2, 4);
+
+    std::ifstream input("data.txt");
+
+    int numVertices;
+    input >> numVertices;
+
+    double x, y;
+
+    while (input.good()) {
+        input >> x >> y;
+        printf( "%f, %f \n", x, y );
+    }
 
     return g;
 }
@@ -168,7 +182,7 @@ int main() {
     std::cout << "Num vertices: " << g.getNumVertices() << std::endl;
     std::cout << "Num edges: " << g.getNumEdges() << std::endl;
 
-    float shortestPathLength = BellmanHeldKarpTsp(g);
+    double shortestPathLength = BellmanHeldKarpTsp(g);
     std::cout << "Shortest travelling salesman path length: " << shortestPathLength << " (" << (int) shortestPathLength
               << ")" << std::endl;
 
